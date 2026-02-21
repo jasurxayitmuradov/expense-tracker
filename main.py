fbcfb80b02a4711689050ad6f12f5b046c5f07ad
jasurxayitmuradov <jxayitmuradov9@gmail.com>
2026-02-21 23:24:14 +0500

    add function

--- main.py
+++ main.py
@@ -1,4 +1,73 @@
 import argparse
+import json
+import os
+from datetime import datetime 
+#the data stucture
+'''
+{
+    "id"int,
+    "date":date,
+    "description":text
+    "amount":float
+}
+'''
+#functions
+
+def get_id():
+    if not os.path.exists("data.json"):
+        return 1
+    try: 
+        with open("data.json" , 'r') as f:
+            content = f.read()
+            if not content:
+                return 1
+            
+            data = json.loads(content)
+
+            if not data:
+                return 1
+            
+            last_id = -1
+            for expense in data:
+                last_id = max(last_id , expense["id"])
+
+            return last_id + 1
+    except (json.JSONDecodeError , KeyError):
+        return 1
+    
+
+def add(description , amount):
+
+    id = get_id()
+
+    if id == 1:
+        data = []
+    else:
+        with open("data.json" , 'r') as f:
+            data = json.load(f)
+
+    new_expense = {
+        "id":id,
+        "date":datetime.now().strftime("%Y-%m-%d %H:%M"),
+        "description":description,
+        "amount":amount
+    }
+
+    data.append(new_expense)
+
+    with open("data.json" , 'w') as f:
+        json.dump(data , f , indent=4)
+    print(f"Expense added successfully (ID: {id})")
+    
+
+def list():
+    pass
+def summary(month=None):
+    pass
+def delete(id):
+    pass
+
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -27,7 +96,7 @@ def main():
     args = parser.parse_args()
 
     if args.command=='add':
-        print(args.description , args.amount)
+        add(args.description , args.amount)
     elif args.command=="list":
         print("Print is working!")
     elif args.command=="summary":
